8395864706653a70c38f89b0375ae36e1b37d2d8
Chris Westin <cwestin@yahoo.com>
2012-10-03 18:32:09 -0700

    refactored variable renderers

--- formatter/src/com/bookofbrilliantthings/mustache4j/FieldRenderer.java
+++ formatter/src/com/bookofbrilliantthings/mustache4j/FieldRenderer.java
@@ -1,28 +1,22 @@
 package com.bookofbrilliantthings.mustache4j;
 
-import java.io.Writer;
 import java.lang.reflect.Field;
 
 public class FieldRenderer
-    implements FragmentRenderer
+    extends VariableRenderer
 {
     private final Field field;
 
     public FieldRenderer(final Field field)
     {
-        final Class<?> theClass = field.getType();
-        final PrimitiveType primitiveType = PrimitiveType.getSwitchType(theClass);
-        if (primitiveType == PrimitiveType.OBJECT)
-            throw new RuntimeException("only primitive type and String fields are supported for now (" + theClass.getName() + ")"); // TODO
-
+        super(field.getType());
         this.field = field;
     }
 
     @Override
-    public void render(final Writer writer, final Object o)
+    public Object getValue(final Object o)
         throws Exception
     {
-        final Object v = field.get(o);
-        writer.write(v.toString());
+        return field.get(o);
     }
 }
---
+++ formatter/src/com/bookofbrilliantthings/mustache4j/VariableRenderer.java
@@ -0,0 +1,28 @@
+package com.bookofbrilliantthings.mustache4j;
+
+import java.io.Writer;
+
+public abstract class VariableRenderer
+    implements FragmentRenderer
+{
+    protected VariableRenderer(Class<?> forClass)
+    {
+        // this constructor is really more for type checking than anything else
+        final PrimitiveType primitiveType = PrimitiveType.getSwitchType(forClass);
+        if (primitiveType == PrimitiveType.OBJECT)
+            throw new RuntimeException("only primitive type and String variables are supported (" +
+                    forClass.getName() + ")");
+    }
+
+    public abstract Object getValue(Object o)
+        throws Exception;
+
+    @Override
+    public void render(final Writer writer, final Object o)
+        throws Exception
+    {
+        final Object value = getValue(o);
+        if (value != null)
+            writer.write(value.toString());
+    }
+}
