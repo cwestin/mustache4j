0caafa1892f8620ec8aac819fd90ab3766ad5ff2
Chris Westin <cwestin@yahoo.com>
2012-10-03 19:37:10 -0700

    refactored referenced object rendering

---
+++ formatter/src/com/bookofbrilliantthings/mustache4j/ObjectSectionRenderer.java
@@ -0,0 +1,60 @@
+package com.bookofbrilliantthings.mustache4j;
+
+import java.io.Writer;
+import java.util.List;
+
+public abstract class ObjectSectionRenderer
+    implements FragmentRenderer
+{
+    private final ObjectRenderer objectRenderer;
+    private final boolean inverted;
+
+    public ObjectSectionRenderer(final List<FragmentRenderer> fragmentList,
+            final boolean inverted, final Class<?> valueClass, final Class<?> containerClass)
+    {
+        final PrimitiveType primitiveType = PrimitiveType.getSwitchType(valueClass);
+        if (primitiveType != PrimitiveType.OBJECT)
+            throw new IllegalArgumentException("section renderer can only operate on non-primitive types");
+
+        objectRenderer = new ObjectRenderer(fragmentList, (inverted ? containerClass : valueClass));
+        this.inverted = inverted;
+    }
+
+    public abstract Object getValue(Object o)
+        throws Exception;
+
+    @Override
+    public void render(final Writer writer, final Object o)
+        throws Exception
+    {
+        final Object value = getValue(o);
+
+        if (!inverted)
+        {
+            if (value == null)
+                return;
+
+            objectRenderer.render(writer, value);
+        }
+        else
+        {
+            if (value == null)
+                objectRenderer.render(writer, o);
+        }
+    }
+
+    protected static abstract class Factory
+        implements RendererFactory
+    {
+        protected final List<FragmentRenderer> fragmentList;
+        protected final boolean inverted;
+
+        Factory(List<FragmentRenderer> fragmentList, boolean inverted)
+        {
+            this.fragmentList = fragmentList;
+            this.inverted = inverted;
+        }
+
+        public abstract FragmentRenderer createRenderer();
+    }
+}
--- formatter/src/com/bookofbrilliantthings/mustache4j/ReferencedObjectRenderer.java
+++ formatter/src/com/bookofbrilliantthings/mustache4j/ReferencedObjectRenderer.java
@@ -1,56 +1,35 @@
 package com.bookofbrilliantthings.mustache4j;
 
-import java.io.Writer;
 import java.lang.reflect.Field;
 import java.util.List;
 
 public class ReferencedObjectRenderer
-    implements FragmentRenderer
+    extends ObjectSectionRenderer
 {
-    private final ObjectRenderer objectRenderer;
-    private final boolean inverted;
     private final Field field;
 
     public ReferencedObjectRenderer(final List<FragmentRenderer> fragmentList,
             final boolean inverted, final Field field)
     {
-        objectRenderer = new ObjectRenderer(fragmentList,
-                (inverted ? field.getDeclaringClass() : field.getType()));
-        this.inverted = inverted;
+        super(fragmentList, inverted, field.getType(), field.getDeclaringClass());
         this.field = field;
     }
 
     @Override
-    public void render(final Writer writer, final Object o)
+    public Object getValue(final Object o)
         throws Exception
     {
-        final Object childObject = field.get(o);
-
-        if (!inverted)
-        {
-            if (childObject == null)
-                return;
-
-            objectRenderer.render(writer, childObject);
-        }
-        else
-        {
-            if (childObject == null)
-                objectRenderer.render(writer, o);
-        }
+        return field.get(o);
     }
 
-    private static class MyRendererFactory
-        implements RendererFactory
+    private static class MyFactory
+        extends Factory
     {
-        private final List<FragmentRenderer> fragmentList;
-        private final boolean inverted;
         private final Field field;
 
-        MyRendererFactory(List<FragmentRenderer> fragmentList, boolean inverted, Field field)
+        MyFactory(List<FragmentRenderer> fragmentList, boolean inverted, Field field)
         {
-            this.fragmentList = fragmentList;
-            this.inverted = inverted;
+            super(fragmentList, inverted);
             this.field = field;
         }
 
@@ -64,6 +43,6 @@ public class ReferencedObjectRenderer
     public static RendererFactory createFactory(final List<FragmentRenderer> fragmentList,
             final boolean inverted, final Field field)
     {
-        return new MyRendererFactory(fragmentList, inverted, field);
+        return new MyFactory(fragmentList, inverted, field);
     }
 }
