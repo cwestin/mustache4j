f12132cc7b4702727c73a4f681b30fd4b5ff762c
Chris Westin <cwestin@yahoo.com>
2012-10-14 11:15:10 -0700

    remove warning

--- test/src/test/bookofbrilliantthings/mustache4j/TestMustache.java
+++ test/src/test/bookofbrilliantthings/mustache4j/TestMustache.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.bookofbrilliantthings.mustache4j.Mustache;
